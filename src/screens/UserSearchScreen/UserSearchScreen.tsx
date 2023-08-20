15482e61ca873c60fc4e7da80bbb2a7b553705bb
suleimanodetoro <odetoro75@gmail>
2023-08-20 11:17:03 +0100

    List users in search screen from api

--- src/screens/UserSearchScreen/UserSearchScreen.tsx
+++ src/screens/UserSearchScreen/UserSearchScreen.tsx
@@ -1,14 +1,42 @@
-import { StyleSheet, Text, View, FlatList } from 'react-native'
-import React from 'react';
-import users from "../../assets/data/users.json";
-import UserListItem from '../../components/UserListItem/';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
+import React from "react";
+import UserListItem from "../../components/UserListItem/";
+
+import { listUsers } from "./queries";
+import { useQuery } from "@apollo/client";
+import ApiErrorMessage from "../../components/ApiErrorMessage/ApiErrorMessage";
+import { ListUsersQuery, ListUsersQueryVariables } from "../../API";
 
 const UserSearchScreen = () => {
-    return (
-        <FlatList data={users} renderItem={({item}) => <UserListItem user={item} />} />
-    )
-}
+  const { data, error, loading, refetch } = useQuery<
+    ListUsersQuery | ListUsersQueryVariables
+  >(listUsers);
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+  if (error) {
+    <ApiErrorMessage
+      title={"User search failed, please retry"}
+      message={error.message}
+    />;
+  }
+  const users = data?.listUsers?.items || [];
+  return (
+    <FlatList
+      data={users}
+      renderItem={({ item }) =>item && <UserListItem user={item} />}
+      onRefresh={refetch}
+      refreshing={loading}
+    />
+  );
+};
 
-export default UserSearchScreen
+export default UserSearchScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
