2a6b5aa069fd8cd6b9b762f641e084a62b11058e
Suleiman Odetoro <odetoro75@gmail.com>
2023-09-03 09:34:43 +0100

    Add authorization restrictions when using application

--- src/apollo/Client.tsx
+++ src/apollo/Client.tsx
@@ -16,8 +16,10 @@ import {
 
 //Import earlier installed packages for using aws appsync with client
 import { createAuthLink, AuthOptions, AUTH_TYPE } from "aws-appsync-auth-link";
-import {createSubscriptionHandshakeLink} from 'aws-appsync-subscription-link';
+import { createSubscriptionHandshakeLink } from "aws-appsync-subscription-link";
 import config from "../aws-exports";
+import { useAuthContext } from "../contexts/AuthContext";
+import { useMemo } from "react";
 
 interface IClient {
   children: React.ReactNode;
@@ -25,26 +27,11 @@ interface IClient {
 
 const url = config.aws_appsync_graphqlEndpoint;
 const region = config.aws_appsync_region;
-const auth: AuthOptions = {
-    type: config.aws_appsync_authenticationType as AUTH_TYPE.API_KEY ,
-    apiKey: config.aws_appsync_apiKey
 
-};
+const httpLink = createHttpLink({ uri: url });
 
-const httpLink = createHttpLink({uri:url})
-
-// In order to group two seperate links (auth link and handshake above) in one, we'll need to use the following =>
-const link = ApolloLink.from([
-       /**
-     * url for 
-     * region of api
-     * authentication
-     */
-       createAuthLink({url, region, auth}),
-       createSubscriptionHandshakeLink({url, region, auth}, httpLink) //second part needs the type of protocol to communicate
-]);
 //Merge List to pass two properties: existing: first query payload, and incoming: second query payload
-const mergeLists = (existing = {items: []}, incoming = {items: []}) => {
+const mergeLists = (existing = { items: [] }, incoming = { items: [] }) => {
   return {
     ...existing,
     ...incoming,
@@ -60,22 +47,56 @@ const typePolicies: TypePolicies = {
       //Policy for loading more comments
       commentsByPost: {
         //Take all items except the fields responsible for pagination
-        keyArgs: ['postID', 'createdAt', 'sortDirection', 'filter'],
+        keyArgs: ["postID", "createdAt", "sortDirection", "filter"],
         merge: mergeLists,
       },
       //Policy for loading more posts
       postsByDate: {
-        keyArgs: ['type', 'createdAt', 'sortDirection', 'filter'],
+        keyArgs: ["type", "createdAt", "sortDirection", "filter"],
         merge: mergeLists,
       },
     },
   },
 };
-const client = new ApolloClient({
-  link, //Use link instea dof providing url directly to client
-  cache: new InMemoryCache({typePolicies}),
-});
+
+//Since the authContext is available to the apollo client(see app.tsx structure)
+//We will move necessary code into the Client component
+//Examplpe of code moved is AuthOptions, link, and client
+
 const Client = ({ children }: IClient) => {
+  const { user } = useAuthContext();
+  const jwtToken =
+    user?.getSignInUserSession()?.getAccessToken().getJwtToken() || "";
+
+  // In order to group two seperate links (auth link and handshake above) in one, we'll need to use the following =>
+
+  //Now that the client is in the body of our component, it'll be created evertime the component is rendered.
+  //To prevent initializing new client after the first render, use memory
+  const client = useMemo(() => {
+    //Rules of this memo is simple -> Whenever the user changes, only then do you store new data about the auth, link, and apollo client
+    const auth: AuthOptions = {
+      type: config.aws_appsync_authenticationType as AUTH_TYPE.AMAZON_COGNITO_USER_POOLS,
+      jwtToken,
+    };
+    const link = ApolloLink.from([
+      /**
+       * url for
+       * region of api
+       * authentication
+       */
+      createAuthLink({ url, region, auth }),
+      createSubscriptionHandshakeLink({ url, region, auth }, httpLink), //second part needs the type of protocol to communicate
+    ]);
+    return new ApolloClient({
+      link, //Use link instead of providing url directly to client
+      cache: new InMemoryCache({ typePolicies }),
+    });
+  }, [user]);
+  // const client = new ApolloClient({
+  //   link, //Use link instea dof providing url directly to client
+  //   cache: new InMemoryCache({typePolicies}),
+  // });
+
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
 };
 export default Client;
